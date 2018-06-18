dd83cbe78b66aa5d33ed9356b3f604f203ee825f
Marcus Weiner <marcus.weiner@gmail.com>
2018-06-18 18:58:21 +0200

    :sparkles: Provide OAuth login on CMS

--- template/cms.jsx
+++ template/cms.jsx
@@ -4,6 +4,7 @@ import netlifyIdentity from 'netlify-identity-widget';
 import HomePage from './pages/home';
 import PreviewProvider from './util/preview-provider';
 
+window.netlifyIdentity = netlifyIdentity;
 netlifyIdentity.init();
 
 CMS.registerPreviewStyle('/cms/site.css');
