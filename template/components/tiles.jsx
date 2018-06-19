1b80838a7d16135d7b6bbecf1f8ac6ea48dfd717
Marcus Weiner <marcus.weiner@gmail.com>
2018-06-19 02:57:18 +0200

    :bug: Fixup Tile image loading and linking

--- template/components/tiles.jsx
+++ template/components/tiles.jsx
@@ -3,26 +3,29 @@ import PropTypes from 'prop-types';
 import { Link } from '../util/link';
 
 export const Tile = props => (
-    <article>
-        {props.imageSource ? (
-            <span className="image">
-                <img src={props.imageSource} alt={props.imageAlt} />
-            </span>
-        ) : null}
+    <article style={props.image ? { backgroundImage: `url(${props.image})` } : {}}>
         <header className="major">
             <h3>
                 <Link to={props.link} className="link">{props.headline}</Link>
             </h3>
             <p>{props.copy}</p>
         </header>
+        <Link to={props.link} className="link primary" />
     </article>
 );
+
 Tile.propTypes = {
-    imageSource: PropTypes.string,
-    imageAlt: PropTypes.string,
-    link: PropTypes.string,
+    image: PropTypes.string,
+    url: PropTypes.string,
     headline: PropTypes.string.isRequired,
     copy: PropTypes.string,
+}
+
+Tile.defaultProps = {
+    image: "",
+    url: "",
+    headline: "",
+    copy: "",
 }
 
 export const TileSection = props => (
