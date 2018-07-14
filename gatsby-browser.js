28d5c2089aa3aafe605c91fc9e4bd6ac563bb91d
Marcus Weiner <marcus.weiner@gmail.com>
2018-07-14 22:31:08 +0200

    :art: Use html.js override to set wrapper classes

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-const ReactDOM = require('react-dom');
-
-exports.replaceHydrateFunction = () => {
-  return (element, container, callback) => {
-    container.id = 'wrapper';
-    container.classList.add('bq-root');
-    ReactDOM.render(element, container, callback);
-  };
-};
