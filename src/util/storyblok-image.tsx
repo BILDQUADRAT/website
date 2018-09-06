03a32eb74f475085b4e1138dc027b6b01279990b
Leo Bernard <admin@leolabs.org>
2018-09-06 04:04:22 +0200

    :sparkles: Implement StoryblokImage for optimized image loading

---
+++ src/util/storyblok-image.tsx
@@ -0,0 +1,54 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import * as React from 'react';
+
+interface StoryblokImageProps {
+    alt: string;
+    aspectRatio: number;
+    className?: string;
+    width: number;
+    height?: number;
+    sizes: string[];
+    smart?: boolean;
+    src: string;
+}
+
+/**
+ * Generates a URL for Storyblok's Image Service
+ * 
+ * Function taken from https://www.storyblok.com/docs/image-service#how-to-use-it
+ * 
+ * @param image image url to resize
+ * @param option <width>x<height>
+ */
+const resize = (image: string, width?: number, height: number = 0, smart: boolean = false) => {
+    var imageService = '//img2.storyblok.com/';
+    var path = image.replace('//a.storyblok.com', '');
+    return imageService + `${width}x${height}` + (smart ? '/smart' : '') + path;
+}
+
+/**
+ * Generates an srcset string for Storyblok images
+ * 
+ * @param image image url to generate an srcset for
+ * @param maxWidth maximum image width that will be used
+ * @param stepSize delta between srcset steps
+ */
+const generateSrcset = (image: string, aspectRatio?: number, smart: boolean = false, maxWidth: number = 3000, stepSize: number = 100) => {
+    return Array.apply(null, {length: Math.floor(maxWidth / stepSize)})
+        .map((e, i) => (i + 1) * stepSize)
+        .map(width => `${resize(image, width, aspectRatio ? width * aspectRatio : 0)} ${width}w`)
+        .join(', ');
+}
+
+const StoryblokImage = (props: StoryblokImageProps) => {
+    return <img
+        alt={props.alt}
+        className={props.className}
+        height={props.height || (props.aspectRatio ? props.width * props.aspectRatio : 0)}
+        src={props.src}
+        width={props.width}
+        srcSet={generateSrcset(props.src, props.aspectRatio, props.smart)}
+        sizes={props.sizes.join(', ')} />;
+}
+
+export default StoryblokImage;
