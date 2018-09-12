d2879d1298c7772caf826ad8163e3253ffe559ad
Moritz Gunz <moritz.gunz@gmail.com>
2018-09-12 16:51:40 +0200

    :sparkles: Implement about-us blok
    Fixes #1

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import Page from "../templates/blok-page";
 import GenericPage from '../templates/generic-page';
 import { Story } from "../util/storyblok";
 
+import { AboutUs, Person } from "./about-us";
 import { Banner } from "./banner";
 import { Button } from "./button";
 import { ContactButton } from "./contact-button";
@@ -24,12 +25,14 @@ export const contentTypes: ComponentMap<ContentTypeProps> = {
 };
 
 export const blocks: ComponentMap<any> = {
+  'about-us': AboutUs,
   banner: Banner,
   button: Button,
   'contact-button': ContactButton,
   feature: Feature,
   'feature-alternating': FeaturesAlternating,
   'fullwidth-image': FullwidthImage,
+  person: Person,
   richtext: Richtext,
   tile: Tile,
   tiles: Tiles,
